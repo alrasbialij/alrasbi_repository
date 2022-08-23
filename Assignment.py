99164e50e0c29c06455e97b9e29492f1edc31da0
alrasbialij <alrasbi.alij@gmail.com>
2022-08-23 07:53:25 +0400

    My Assignment

---
+++ Assignment.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+#  **ALI Al RASBI**
+#  <br> Data Expert  </br>
+
+# <i> I am interested in data science because there is more dmand on it! </i>
+
+# ### The code in cell 5 is supposed to find the result of 5 multiply by 7
+
+# In[1]:
+
+
+5 * 7
+
+
+# 
+# My Jupyter Notebook on IBM Watson Studio
+# ------------
+# 
+# | ID | Name |
+# |----|-----|
+# | 1 | Ali |
+# | 2 | Juma |
+# 
+# * Saturday
+# * sunday
+# * Monday
+# 
+# 1. Saturday
+# 2. Sunday
+# 3. Monday
+# 
+# __[Google](http://www.google.com)__
+# 
+
+# In[ ]:
+
+
+
+
